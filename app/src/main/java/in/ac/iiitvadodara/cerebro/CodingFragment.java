cb38be5b4f7ecb6e2f0e7825124d6b8669ec6551
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-02-28 11:05:50 +0530

    Resolved dead links (#29)

--- app/src/main/java/in/ac/iiitvadodara/cerebro/CodingFragment.java
+++ app/src/main/java/in/ac/iiitvadodara/cerebro/CodingFragment.java
@@ -1,11 +1,14 @@
 package in.ac.iiitvadodara.cerebro;
 
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -22,8 +25,18 @@ public class CodingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_coding, container, false);
+        ImageView image = v.findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), EventInfoActivity.class);
+                startActivity(i);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coding, container, false);
+        return v;
     }
 
 }
